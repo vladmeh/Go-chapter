7387e28d71bbc587d4e3a6c9acf4f97f10fdb214
vladmeh <vladmeh@gmail.com>
2016-10-15 00:20:24 +0300

    Chapter 9. Интерфейсы

--- chapter9/main.go
+++ chapter9/main.go
@@ -10,6 +10,20 @@ type Rectangle struct {
     x1, y1, x2, y2 float64
 }
 
+type Person struct {
+    Name string
+}
+
+type Android struct {
+    Person
+    Model string
+}
+
+type Shape interface {
+    area() float64
+    perim() float64
+}
+
 func distance(x1, y1, x2, y2 float64) float64 {
     a := x2 - x1
     b := y2 - y1
@@ -21,19 +35,51 @@ func (r *Rectangle) area() float64 {
     return l * w
 }
 
+func (r *Rectangle) perim() float64 {
+    l := distance(r.x1, r.y1, r.x1, r.y2)
+    w := distance(r.x1, r.y1, r.x2, r.y1)
+    return 2*l + 2*w
+}
+
 func (c *Circle) area() float64 {
     return math.Pi * c.r*c.r
 }
 
-func (c *Circle) length() float64 {
+func (c *Circle) perim() float64 {
     return math.Pi*2 * c.r
 }
 
-func main() {
-    r := Rectangle{0, 0, 10, 10}
-    fmt.Println("Площадь прямоугольника:", r.area())
+func (p *Person) Talk() {
+    fmt.Println("Hi, my name is", p.Name)
+}
 
+func totalCalc(shapes ...Shape) (float64, float64) {
+    var area float64
+    var perim float64
+    for _, s := range shapes {
+        area += s.area()
+        perim += s.perim()
+    }
+    return area, perim
+}
+
+func main() {
+    r := Rectangle{0, 0, 10, 5}
+    fmt.Println("Площадь прямоугольника:", r.area())
+    fmt.Println("Периметр прямоугольника:", r.perim())
+    fmt.Println()
     c := Circle{0, 0, 5}
     fmt.Println("Площадь круга:", c.area())
-    fmt.Println("Длина окружности:", c.length())
+    fmt.Println("Перимерт окружности:", c.perim())
+    fmt.Println()
+    ta, tp := totalCalc(&c, &r)
+    fmt.Println("Общая площадь всех фигур:", ta)
+    fmt.Println("Oбщий периметр всех фигур:", tp)
+
+    fmt.Println()
+    n := Person{"Вася"}
+    //a := new(Android)
+    a := Android{n, "Пупкин"}
+    fmt.Println(a.Name, a.Model)
+    a.Talk()
 }
