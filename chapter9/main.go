26bd38b86a943c6a5cb885092aedf1b7fc3a6dce
VladMeh <vladmeh@gmail.com>
2016-10-14 17:39:23 +0300

    chapter9 Методы

--- chapter9/main.go
+++ chapter9/main.go
@@ -6,14 +6,18 @@ type Circle struct {
     x, y, r float64
 }
 
+type Rectangle struct {
+    x1, y1, x2, y2 float64
+}
+
 func distance(x1, y1, x2, y2 float64) float64 {
     a := x2 - x1
     b := y2 - y1
     return math.Sqrt(a*a + b*b)
 }
-func rectangleArea(x1, y1, x2, y2 float64) float64 {
-    l := distance(x1, y1, x1, y2)
-    w := distance(x1, y1, x2, y1)
+func (r *Rectangle) area() float64 {
+    l := distance(r.x1, r.y1, r.x1, r.y2)
+    w := distance(r.x1, r.y1, r.x2, r.y1)
     return l * w
 }
 
@@ -26,9 +30,8 @@ func (c *Circle) length() float64 {
 }
 
 func main() {
-    var rx1, ry1 float64 = 0, 0
-    var rx2, ry2 float64 = 10, 10
-    fmt.Println(rectangleArea(rx1, ry1, rx2, ry2))
+    r := Rectangle{0, 0, 10, 10}
+    fmt.Println("Площадь прямоугольника:", r.area())
 
     c := Circle{0, 0, 5}
     fmt.Println("Площадь круга:", c.area())
